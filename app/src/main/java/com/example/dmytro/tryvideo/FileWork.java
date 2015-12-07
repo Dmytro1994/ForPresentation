f9b1a5b830477f651594f8595aaa6bac0786692a
Dmytro Ubogiy <dmytroubogiy@gmail.com>
2015-12-07 13:20:41 +0200

    tag2

--- app/src/main/java/com/example/dmytro/tryvideo/FileWork.java
+++ app/src/main/java/com/example/dmytro/tryvideo/FileWork.java
@@ -14,6 +14,7 @@ import java.io.File;
     //branchforrebase first change
     //branchforrebase second change
     //fdgsdfg
+    //taG2
 public class FileWork {
     private static File mediaFile;
     private static File storageMedia;
