595f273630370edb1e4c932be874da519e30c77a
Dmytro Ubogiy <dmytroubogiy@gmail.com>
2015-12-07 13:02:41 +0200

    Revert "branchforrebase second change"
    This reverts commit 8621baf07ee58160a4c68c519230f980633ac114.

--- app/src/main/java/com/example/dmytro/tryvideo/FileWork.java
+++ app/src/main/java/com/example/dmytro/tryvideo/FileWork.java
@@ -12,7 +12,6 @@ import java.io.File;
     //first dev commit
     //second dev commit
     //branchforrebase first change
-    //branchforrebase second change
 public class FileWork {
     private static File mediaFile;
     private static File storageMedia;
